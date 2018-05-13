d1020f08ee72a04a235c8cce9723e54313021a9a
sakochik <sakochi.kodai1209@gmail.com>
2018-05-13 14:18:51 +0900

    add: storybook関連addon

--- stories/index.js
+++ stories/index.js
@@ -1,8 +1,12 @@
 import { storiesOf } from '@storybook/vue';
+import { LinksTo } from '@storybook/addon-links';
+import { withKnobs, text, color } from '@storybook/addon-knobs';
+import Centered from '@storybook/addon-centered';
 
 import composeButton from '../components/composeButton.vue';
 
 storiesOf('composeButton', module)
+  .addDecorator(Centered)
   .add('simple', () => ({
     components: { composeButton },
     template: '<composeButton></composeButton>',
