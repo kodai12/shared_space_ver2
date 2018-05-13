608d573a7973e780260b36f8338bbb5ef22328f6
sakochik <sakochi.kodai1209@gmail.com>
2018-05-13 14:45:38 +0900

    add: UserNameListコンポーネント

---
+++ components/UserNameList.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul class="user_name-list">
+    <li class="item"
+      v-for="name in names">
+      <a href="#">
+        <span>{{ name }}</span>
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      names: [
+        'sample name1',
+        'sample name2'
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- stories/index.js
+++ stories/index.js
@@ -4,10 +4,18 @@ import { withKnobs, text, color } from '@storybook/addon-knobs';
 import Centered from '@storybook/addon-centered';
 
 import composeButton from '../components/composeButton.vue';
+import UserNameList from '../components/UserNameList.vue';
 
 storiesOf('composeButton', module)
   .addDecorator(Centered)
   .add('simple', () => ({
     components: { composeButton },
     template: '<composeButton></composeButton>',
+  }));
+
+storiesOf('UserNameList', module)
+  .addDecorator(Centered)
+  .add('simple', () => ({
+    components: { UserNameList },
+    template: '<UserNameList></UserNameList>',
   }));
